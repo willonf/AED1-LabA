6ebbd4eeea4cb48dd8ca3105055a24123d642879
Willon Ferreira <contatowillon@gmail.com>
2021-06-11 06:50:54 -0400

    TP2

--- AED1/00 Trabalhos Práticos/TP2/TorreDeHanoi.h
+++ AED1/00 Trabalhos Práticos/TP2/TorreDeHanoi.h
@@ -1,13 +1,14 @@
 #define torreDeHanoi_h
 
+typedef struct disco Disco;
+typedef struct pino Pino;
+
 Pino *criarPino();
 Disco *criarDisco(int tam);
 Disco *pop(Pino *pino);
 void push(Pino *pino, Disco *disco);
 void excluirPino(Pino *pino);
 
-typedef struct disco Disco;
-typedef struct pino Pino;
 struct disco
 {
     char tamDisco;
