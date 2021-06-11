3534b7226034e434fc5ecd43ae3122dbf5cc0804
Willon Ferreira <contatowillon@gmail.com>
2021-06-11 08:36:26 -0400

    LAB A

--- AED1/00 Trabalhos Práticos/TP2/compilar.sh
+++ AED1/00 Trabalhos Práticos/TP2/compilar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 echo "Iniciando o executável..."
-gcc -c TP2_Willon_Ferreira_da_Silva.c
+gcc -c main.c
 gcc -c TorreDeHanoi.c
 gcc -o TP2 *.o
 rm -rf *.o
--- Laboratório A/11 - Recursão 2/02.c
+++ Laboratório A/11 - Recursão 2/02.c
@@ -8,6 +8,7 @@ int main(void)
 	char numero[101];
 
 	scanf("%s", numero);
+
 	while (numero[0] != 'f')
 	{
 		if (trilegal(numero))
@@ -23,25 +24,64 @@ int main(void)
 	}
 }
 
+void cp_parte(char *string, int inicio, int fim, char *parte)
+{
+	int origem = inicio;
+	int destino = 0;
+	while (origem <= fim)
+	{
+		parte[destino] = string[origem];
+		destino++;
+		origem++;
+	}
+	parte[destino] = 0;
+}
+
+int somaDigitos(char *num)
+{
+	int soma = 0;
+	int pos = 0;
+	while (pos != 0)
+	{
+		soma += num[pos] - '0';
+		if (soma >= 10)
+		{
+			soma = soma % 10;
+		}
+		pos++;
+	}
+	return soma;
+}
+
 int trilegal(char numero[])
 {
-	if (strlen(numero) % 3 != 0)
+	char size = strlen(numero);
+	char *p1 = &numero[40];
+	char *p2 = &numero[40];
+	char *p3 = &numero[40];
+
+	if (size % 3 != 0)
+	{
+		if (size == 1)
+		{
+			return 1;
+		}
+		return 0;
+	}
+	cp_parte(numero, 0, size / 3 - 1, p1);
+	cp_parte(numero, size / 3, 2 * size / 3 - 1, p2);
+	cp_parte(numero, 2 * size / 3, size - 1, p3);
+	if (strlen(p1) % 3 != 0 || strlen(p2) % 3 != 0 || strlen(p3) % 3 != 0)
 	{
 		return 0;
 	}
 	else
 	{
-		int digitos = strlen(numero) / 3;
-		char p[digitos];
-		while (true)
+		
+		if (somaDigitos)
 		{
-
-			for (int i = 0; i < 3; i++)
-			{
-				strcat(p, numero[i]);
-			}
+			/* code */
 		}
-
-		return 1;
 	}
+	return 1;
 }
