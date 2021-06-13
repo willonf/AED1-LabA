c7eb061e9a8ade9c43a41313d979da538083e3cc
Willon Ferreira <contatowillon@gmail.com>
2021-06-13 16:29:04 -0400

    Lab A

--- Laboratório A/11 - Recursão 2/02.c
+++ Laboratório A/11 - Recursão 2/02.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int trilegal(char numero[]);
 int main(void)
@@ -19,15 +20,16 @@ int main(void)
 		{
 			printf("Chato.\n");
 		}
-
 		scanf("%s", numero);
 	}
+	trilegal(numero);
 }
 
 void cp_parte(char *string, int inicio, int fim, char *parte)
 {
 	int origem = inicio;
 	int destino = 0;
+
 	while (origem <= fim)
 	{
 		parte[destino] = string[origem];
@@ -35,21 +37,37 @@ void cp_parte(char *string, int inicio, int fim, char *parte)
 		origem++;
 	}
 	parte[destino] = 0;
+	printf("Parte: %s\n", parte);
 }
 
 int somaDigitos(char *num)
 {
+	int size = strlen(num);
+	int cont = 0;
 	int soma = 0;
 	int pos = 0;
-	while (pos != 0)
+	while (num[pos] != 0)
 	{
 		soma += num[pos] - '0';
+		pos++;
+	}
+	printf("Soma digitos: %d\n", soma);
+	return soma;
+}
+
+int somaNumeros(char *num1, char *num2)
+{
+	int size = strlen(num1);
+	int soma = 0;
+	for (int i = 0; i < size; i++)
+	{
+		soma += (num1[i]-'0') + (num2[i] - '0');
 		if (soma >= 10)
 		{
 			soma = soma % 10;
 		}
-		pos++;
 	}
+	printf("Soma numeros: %d\n", soma);
 	return soma;
 }
 
@@ -68,20 +86,20 @@ int trilegal(char numero[])
 		}
 		return 0;
 	}
-	cp_parte(numero, 0, size / 3 - 1, p1);
-	cp_parte(numero, size / 3, 2 * size / 3 - 1, p2);
-	cp_parte(numero, 2 * size / 3, size - 1, p3);
-	if (strlen(p1) % 3 != 0 || strlen(p2) % 3 != 0 || strlen(p3) % 3 != 0)
-	{
-		return 0;
-	}
 	else
 	{
-		
-		if (somaDigitos)
+		cp_parte(numero, 0, (size / 3) - 1, p1);
+		cp_parte(numero, size / 3, 2 * (size / 3) - 1, p2);
+		cp_parte(numero, 2 * (size / 3), size - 1, p3);
+		printf("Int P3: %d\n\n", atoi(p3));
+		int intP3 = atoi(p3);
+		bool cond1 = somaDigitos(p1) == somaDigitos(p2);
+		bool cond2 = somaNumeros(p1, p2) == intP3;
+
+		if (trilegal(p1) && trilegal(p2) && trilegal(p3) && cond1 && cond2)
 		{
-			/* code */
+			return 1;
 		}
+		return 0;
 	}
-	return 1;
 }
