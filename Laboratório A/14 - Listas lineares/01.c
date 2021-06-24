2f56e9fe7b92310eb58f4b19dca5a455dfe7bdf5
Willon Ferreira <contatowillon@gmail.com>
2021-06-23 21:34:14 -0400

    Lab A

--- Laboratório A/14 - Listas lineares/01.c
+++ Laboratório A/14 - Listas lineares/01.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct lista
 {
@@ -12,44 +13,67 @@ void lista_inserir(Lista *lista, int pos, int novo)
 {
     lista->capacidade++;
     lista->dados = realloc(lista->dados, (lista->capacidade) * sizeof(int));
-    for (int i = lista->tamanho; i > pos; i--)
-    {
-        lista->dados[i] = lista->dados[i - 1];
-    }
+    // for (int i = lista->tamanho; i >= pos; i--)
+    // {
+    //     lista->dados[i] = lista->dados[i - 1];
+    // }
     lista->dados[pos] = novo;
     lista->tamanho++;
-
-    // lista->tamanho++;
-    // printf("\n%d\n\n", lista->tamanho);
-    // printf("Tamanho: %d\n", lista->tamanho);
-    // lista->dados[lista->tamanho-1] = novo;
-    // printf("Oi");
 }
-
-void lista_remover(Lista *lista, int pos)
-{
-    while (pos < lista->tamanho - 1)
-    {
-        lista->dados[pos] = lista->dados[pos + 1];
-        pos++;
-    }
-    lista->tamanho--;
-}
-
-
 
 void main(int argc, char const *argv[])
 {
     int valor;
+    int existe;
     Lista *lista = malloc(sizeof(Lista));
-    lista->capacidade = 1;
-    lista->tamanho = 1;
+    lista->capacidade = 10;
+    lista->tamanho = 0;
     lista->dados = malloc(lista->capacidade * sizeof(int));
-    
-    while(1) {
-        scanf("%d", valor);
-        lista_inserir(lista, )
+    int pos = 0;
+    while (true)
+    {
+        scanf("%d", &valor);
+        if (!valor)
+        {
+            break;
+        }
+        lista_inserir(lista, pos, valor);
+        pos++;
     }
-
-    lista_inserir(lista, lista->capacidade, 3);
+    while (true)
+    {
+        pos = 0;
+        existe = -1;
+        scanf("%d", &valor);
+        if (!valor)
+        {
+            exit(1);
+        }
+        while (pos <= lista->tamanho - 1)
+        {
+            if (valor == lista->dados[pos])
+            {
+                existe = pos;
+                break;
+            }
+            pos++;
+        }
+        if (existe == 0)
+        {
+            printf("%d: fim", valor);
+        }
+        else if (existe == lista->tamanho - 1)
+        {
+            printf("%d: inicio", valor);
+        }
+        else if (existe > 0 && existe < lista->tamanho - 1)
+        {
+            printf("%d: existe", valor);
+        }
+        else if (existe < 0)
+        {
+            printf("%d: nao existe", valor);
+        }
+        printf("\n");
+    }
 }
--- Laboratório A/14 - Listas lineares/02.c
+++ Laboratório A/14 - Listas lineares/02.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
 
-struct Musica {
+typedef struct musica {
 	char autor[100];
 	char titulo[100];
 	char arquivo[100];
 	int duracao;
-};
+} Musica;
 
-struct ListaMusicas {
-	struct Musica *dados;
+typedef struct listaMusicas {
+	Musica *dados;
 	int tamanho;
 	int capacidade;
-};
+} ListaMusicas;
 
 // Cria uma nova lista vazia
 struct ListaMusicas lista_criar(int capacidade);
 
 // Insere uma música no fim
-void lista_inserir_fim(struct ListaMusicas *lista, struct Musica novo);
+void lista_inserir_fim(ListaMusicas *lista, Musica novo);
 
 // Verifica se uma música existe na lista; retorna o índice ou -1
-int lista_buscar(struct ListaMusicas *lista, char *artista, char *titulo);
+int lista_buscar(ListaMusicas *lista, char *artista, char *titulo);
 
 // Le uma nova musica do teclado e retorna um registro para ela
-struct Musica ler_musica(void);
+Musica ler_musica(void);
 
 int main(void)
 {
-	struct ListaMusicas *base;
-	struct ListaMusicas *playlist;
-	
+	ListaMusicas *base;
+	ListaMusicas *playlist;
 
 	return 0;
 }
