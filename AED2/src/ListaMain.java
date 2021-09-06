afdb23f453f1573e6294f9f1f9febfc47ecdb16f
Willon Ferreira <contatowillon@gmail.com>
2021-09-06 12:47:56 -0400

    Trabalho prático

--- AED2/src/ListaMain.java
+++ AED2/src/ListaMain.java
@@ -1,29 +1,16 @@
-import listas.ListaDuplamenteEncadeada;
-import listas.No2;
+import trabalhopratico1.StringEncadeada;
 
 public class ListaMain {
 
     public static void main(String[] args) {
-        ListaDuplamenteEncadeada lista = new ListaDuplamenteEncadeada();
-        lista.insereInicio("Doctor Who");
-        lista.insereInicio("Daleks");
-        lista.insereInicio("Cybermen");
-        lista.insereInicio("Weeping Angels");
-        No2 noTopo = lista.getTopo();
-        No2 noTopoProx = noTopo.getProx();
-        Object[] listaVetor = lista.vetor();
-        ListaDuplamenteEncadeada lista2 = new ListaDuplamenteEncadeada();
-        lista2.insereInicio("River Song");
-        lista2.insereInicio("Melody Pond");
-        lista.concatena(lista2);
-        lista.insereIndice(1, "The power of the Daleks");
-        lista.imprime();
-        System.out.println("+====+++=====+++=====++");
-        System.out.println(lista.get(1));
-        System.out.println("+====+++=====INVERSO+++=====++");
-        lista.imprimeInverso();
-        lista.limpa();
-        lista2.limpa();
+        StringEncadeada world8  = new StringEncadeada("Omicron Persei 8");
+//        StringEncadeada world9  = new StringEncadeada("Amphibios 9");
+//        StringEncadeada world10 = new StringEncadeada("Decapod 10");
+//        StringEncadeada world11 = new StringEncadeada(new char[]{'W', 'i', 'l'});
+//        StringEncadeada world11 = new StringEncadeada(new StringEncadeada("Willon"));
+//        System.out.println(world8.getTopo().getCaractere());
+//        System.out.println(world8.getCauda().getCaractere());
+        System.out.println(world8.charAt(3));;
     }
 }
 
--- AED2/src/trabalhopratico1/StringEncadeada.java
+++ AED2/src/trabalhopratico1/StringEncadeada.java
@@ -6,9 +6,121 @@ public class StringEncadeada {
     private StringNo cauda;
     private int tamanho;
 
-    public StringEncadeada() {}
-    public StringEncadeada(String conteudo) {}
-    public StringEncadeada(char[] conteudo) {}
-    public StringEncadeada(StringEncadeada conteudo) {}
+    public StringEncadeada() {
+        this.topo = null;
+        this.cauda = null;
+        this.tamanho = 0;
+    }
+
+    public StringEncadeada(String conteudo) {
+        for (int i = 0; i < conteudo.length(); i++) {
+            this.insereFim(conteudo.charAt(i));
+        }
+
+    }
+
+    public StringEncadeada(char[] conteudo) {
+        for (char c : conteudo) {
+            this.insereFim(c);
+        }
+    }
+
+    public StringEncadeada(StringEncadeada conteudo) {
+        this.tamanho = conteudo.length();
+        this.topo = conteudo.getTopo();
+        this.cauda = conteudo.getCauda();
+    }
+
+    public StringNo getTopo() {
+        return this.topo;
+    }
+
+    public StringNo getCauda() {
+        return this.cauda;
+    }
+
+    public void insereInicio(char c) {
+        StringNo newChar = new StringNo(c);
+        newChar.setProx(this.getTopo());
+
+        if (this.getTopo() != null) {
+            this.topo.setAnt(newChar);
+        } else {
+            this.cauda = newChar;
+        }
+        this.topo = newChar;
+        this.tamanho++;
+    }
+
+    public void insereFim(char c) {
+        StringNo newChar = new StringNo(c);
+        newChar.setProx(null);
+
+        if (this.getTopo() == null) {
+            this.topo = newChar;
+        } else {
+            newChar.setAnt(this.getCauda());
+            this.cauda = newChar;
+        }
+        this.tamanho++;
+
+    }
+
+    public boolean isEmpty() {
+        return this.length() == 0;
+    }
+
+    public int length() {
+        return this.tamanho;
+    }
+
+    public char charAt(int index) {
+        if (index < 0 || index >= this.tamanho) {
+            return 0;
+        }
+        StringNo noAtual = this.getTopo();
+
+        while (index-- > 0) {
+            noAtual = noAtual.getProx();
+        }
+        return noAtual.getCaractere();
+    }
+
+    public int indexOf(int ch) {
+        int index = 0;
+        boolean contains = false;
+        for (StringNo noAtual = this.getTopo(); noAtual != null; noAtual = noAtual.getProx()) {
+            System.out.println(noAtual.getCaractere());
+        }
+        return index;
+    }
+
+    public boolean equals(Object str) {
+        return true;
+    }
+
+    public void concat(StringEncadeada str) {
+        this.cauda.setProx(str.getTopo());
+        this.cauda = str.cauda;
+        str.topo.setAnt(this.cauda.getAnt());
+        this.tamanho = this.length() + str.tamanho;
+    }
+
+    public void replace(char oldChar, char newChar) {
+
+    }
+
+    public int compareTo(StringEncadeada anotherString) {
+        return 1;
+    }
+
+    public void toStr() {
+        StringBuilder result = new StringBuilder("");
+        for (StringNo noAtual = this.getTopo(); noAtual != null; noAtual = noAtual.getProx()) {
+            System.out.println(noAtual.getCaractere());
+        }
+//        return result.toString();
+    }
+
 
 }
