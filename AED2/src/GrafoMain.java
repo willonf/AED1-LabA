7c69e482bce19535d79267e73d1de838898b353c
Willon Ferreira <contatowillon@gmail.com>
2021-11-17 19:20:46 -0400

    Grafos

--- AED2/src/GrafoMain.java
+++ AED2/src/GrafoMain.java
@@ -3,8 +3,21 @@ import graphs.GrafoListaAdjacencias;
 public class GrafoMain {
 
     public static void main(String[] args) {
-        GrafoListaAdjacencias grafo = new GrafoListaAdjacencias(6);
-
+        GrafoListaAdjacencias grafo = new GrafoListaAdjacencias(4);
+        grafo.getVertice(0).setX(0);
+        grafo.getVertice(0).setY(0);
+        grafo.getVertice(1).setX(1);
+        grafo.getVertice(1).setY(0);
+        grafo.getVertice(2).setX(2);
+        grafo.getVertice(2).setY(0);
+        grafo.getVertice(3).setX(3);
+        grafo.getVertice(3).setY(0);
+        grafo.insereArestaSimetrica(0, 1, 1);
+        grafo.insereArestaSimetrica(1, 2, 1);
+        grafo.insereArestaSimetrica(2, 3, 1);
+        int[] caminho;
+// Exemplo dos slides (Dijkstra)
+        grafo = new GrafoListaAdjacencias(8);
         grafo.getVertice(0).setX(0);
         grafo.getVertice(0).setY(1);
         grafo.getVertice(1).setX(1);
@@ -12,28 +25,31 @@ public class GrafoMain {
         grafo.getVertice(2).setX(1);
         grafo.getVertice(2).setY(2);
         grafo.getVertice(3).setX(2);
-        grafo.getVertice(3).setY(0);
-        grafo.getVertice(4).setX(2);
-        grafo.getVertice(4).setY(2);
-        grafo.getVertice(5).setX(3);
+        grafo.getVertice(3).setY(1);
+        grafo.getVertice(4).setX(4);
+        grafo.getVertice(4).setY(1);
+        grafo.getVertice(5).setX(5);
         grafo.getVertice(5).setY(0);
-
+        grafo.getVertice(6).setX(5);
+        grafo.getVertice(6).setY(2);
+        grafo.getVertice(7).setX(6);
+        grafo.getVertice(7).setY(1);
         grafo.insereArestaSimetrica(0, 1, 2);
-        grafo.insereArestaSimetrica(0, 2, 8);
-        grafo.insereArestaSimetrica(1, 2, 15);
-        grafo.insereArestaSimetrica(1, 3, 3);
-        grafo.insereArestaSimetrica(2, 4, 2);
-        grafo.insereArestaSimetrica(3, 4, 4);
-        grafo.insereArestaSimetrica(3, 5, 45);
-
-        System.out.println(grafo);
-
-        int[] caminho = grafo.caminhoGuloso(0, 4);
-
-        System.out.print("Caminho guloso entre 0 e 4: ");
-        if (caminho != null)
-            for (int i=0; i<caminho.length; i++)
-                System.out.print(caminho[i] + " ");
+        grafo.insereArestaSimetrica(0, 2, 6);
+        grafo.insereArestaSimetrica(1, 3, 2);
+        grafo.insereArestaSimetrica(1, 5, 7);
+        grafo.insereArestaSimetrica(2, 3, 1);
+        grafo.insereArestaSimetrica(2, 6, 4);
+        grafo.insereArestaSimetrica(3, 4, 2);
+        grafo.insereArestaSimetrica(4, 5, 3);
+        grafo.insereArestaSimetrica(4, 6, 2);
+        grafo.insereArestaSimetrica(5, 7, 3);
+        grafo.insereArestaSimetrica(6, 7, 2);
+        caminho = grafo.caminhoGuloso(0, 5);
+        System.out.print("Caminho guloso entre 0 e 5: ");
+        if (caminho != null) {
+            for (int j : caminho) System.out.print(j + " ");
+        }
     }
 }
 
--- AED2/src/graphs/GrafoListaAdjacencias.java
+++ AED2/src/graphs/GrafoListaAdjacencias.java
@@ -64,6 +64,65 @@ public class GrafoListaAdjacencias {
         numVertices--;
     }
 
+    public void removeArestaSimetrica(int v1, int v2) {
+        removeAresta(v1, v2);
+        removeAresta(v2, v1);
+    }
+
+    public int[] caminhoGuloso(int origem, int destino) {
+        int tamCaminho = 0;
+        int[] caminho = new int[this.getNumVertices()];
+        boolean[] visitado = new boolean[this.getNumVertices()];
+
+        Vertice vDestino = this.getVertice(destino);
+        Vertice vizinho;
+        VerticeAdjacente vizinhos;
+        Vertice melhorVizinho = null;
+        double dist;
+        int menorDistancia = Integer.MAX_VALUE;
+
+        double xDestino = this.getVertice(destino).getX();
+        double yDestino = this.getVertice(destino).getY();
+
+        for (int i = 0; i < this.getNumVertices(); i++) {
+            caminho[i] = -1;
+            visitado[i] = false;
+        }
+
+        caminho[tamCaminho++] = origem;
+        Vertice noAtual = this.getVertice(origem);
+
+        while (noAtual != vDestino) {
+            vizinhos = noAtual.getVizinhos();
+            do {
+                if (!visitado[vizinhos.getVertice().getId()]) {
+                    vizinho = vizinhos.getVertice();
+                    double xAtual = vizinho.getX();
+                    double yAtual = vizinho.getY();
+                    dist = Math.sqrt(Math.pow(xAtual-xDestino, 2) + Math.pow(yAtual - yDestino, 2));
+                    if(dist < menorDistancia){
+                        melhorVizinho = vizinho;
+                        menorDistancia = (int) dist;
+                    }
+                }
+                vizinhos = vizinhos.getProx();
+            } while (vizinhos != null);
+
+
+            if (melhorVizinho == null) {
+                return null;
+            }
+            caminho[tamCaminho++] = melhorVizinho.getId();
+            visitado[melhorVizinho.getId()] = true;
+            noAtual = melhorVizinho;
+        }
+
+
+        return caminho;
+    }
+
+
+
     public int getNumVertices() {
         return numVertices;
     }
