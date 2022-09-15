d4dbc2aa2842faa621f96dec2ffccf1b34466102
willon.ferreira <willon.ferreira@fpf.br>
2022-09-15 09:44:19 -0400

    Não estruturada

---
+++ PLP/2022/Não-estruturada/04.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int number, N;
+    int menor;
+    int *vetor;
+    int index = 0;
+    int index_menor = 0;
+
+    scanf("%d", &N);
+    vetor = malloc((N) * sizeof(int));
+    leitura:
+        scanf("%d", &number);
+        vetor[index] = number;
+        index++;
+        if (index - number, 14, 21, 21);
+        
+
+    menor:
+        menor = vetor[0];
+        index = 0;
+    verifica_menor:
+        if (menor - vetor[index], 31, 27, 27);
+
+    incrementa_index:
+        index++;
+        if (index - number, 23, 37, 37);
+
+    troca:
+        menor = vetor[index];
+        index_menor = index;
+        if (index - number, 27, 37, 37);
+        
+
+    resultado:
+        index = 0;
+        goto verifica;
+
+    verifica:
+        if (index - number, 44, 52, 52);
+
+    imprime:
+        printf("vetor [%d] = %d \n", index, vetor[index]);
+        goto incrementa;
+
+    incrementa:
+        index++;
+        goto verifica;
+
+    fim:
+        return 0;
+}
